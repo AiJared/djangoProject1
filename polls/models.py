fcd00cb789563eca39db18f8c43d07cb2c0098e0
Kerriea-star <ojared511@gmail.com>
2021-07-23 22:02:05 +0300

    Models

--- polls/models.py
+++ polls/models.py
@@ -6,6 +6,6 @@ class Question(models.Model):
     pub_date = models.DateField('date published')
 
 class Choice(models.Model):
-    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
